9a3bfa4070a394db1a804bdda81902826350c831
Grant Hutchinson <grant@splorp.com>
2013-11-25 10:46:54 -0700

    Post formatting

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Tersus
+ */
+?>
+
+<?php get_header(); ?>
+
+<section id="content">
+<?php if (have_posts()) : ?>
+
+	<?php while (have_posts()) : the_post(); ?>
+
+		<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+		
+			<?php if(!has_post_format('aside') && !has_post_format('image')) { ?>
+				<h2 class="entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent link to “<?php the_title_attribute(); ?>”"><?php the_title(); ?></a></h2>
+				<p>By <span class="vcard author"><cite class="fn"><a class="url" href="<?php the_author_meta('user_url') ?>" title="Visit the author’s site"><?php the_author_meta('display_name'); ?></a></cite> | Published: <span class="published" title="<?php the_time('c') ?>"><?php the_date() ?></span></p>
+
+			<?php }
+				the_content();
+				if(!has_post_format('aside') && !has_post_format('image')) {
+			?>
+				
+				<?php if (has_tag()) { ?>
+					<h6>Tags</h6>
+					<p><?php the_tags( '', ', ', '' ) ?></p>
+				<?php } ?>
+				<h6>Categories</h6>
+				<p><?php the_category( ', ' ) ?></p>
+			<?php } ?>
+			
+			<p><a href="<?php the_permalink(); ?>#comment" title="Contribute to the discussion"><?php comments_number('No Comments', '1 Comment', '% Comments'); ?></a></p>
+			<?php edit_post_link('Edit', '<p>', '</p>'); ?>
+		</article>
+
+	<?php endwhile; ?>
+
+	<?php if (show_posts_link_nav()): ?>
+		<p><?php next_posts_link('Older'); delim_posts_link(); previous_posts_link('Newer') ?></p>
+	<?php endif; ?>
+
+<?php else : ?>
+
+	<h2>Not found.</h2>
+	<p>Sorry, you seem to be looking for something that simply isn’t here.</p>
+
+<?php endif; ?>
+</section>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
---
+++ single.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Tersus
+ */
+?>
+
+<?php get_header(); ?>
+
+<section id="content">
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+<?php if (show_post_link_nav()): ?>
+	<p><?php previous_post_link('%link'); delim_post_link(); next_post_link('%link') ?></p>
+<?php endif; ?>
+
+	<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+
+		<h2 class="entry-title"><?php if(the_title( '', '', false ) !='') the_title(); else echo 'Untitled';?></h2>
+
+		<p>By <span class="vcard author"><cite class="fn"><a class="url" href="<?php the_author_meta('user_url') ?>" title="Visit the author’s site"><?php the_author_meta('display_name'); ?></a></cite></span> | Published: <span class="published" title="<?php the_time('c') ?>"><?php the_date() ?></span></p>
+
+		<?php the_content(); ?>
+		<?php wp_link_pages(array('before' => '<p>Pages: ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
+
+		<?php if (has_tag()) echo '<p>Tags:</p>'; the_tags('<ul><li>','</li><li>','</li></ul>'); ?>
+
+		<p>Categories:</p>
+		<ul>
+			<li><?php the_category('</li><li>') ?></li>
+		</ul>
+
+		<?php if ( comments_open() && pings_open() ) { ?>
+		<p><a href="#comment" title="Contribute to the discussion">Leave a comment</a> or <a href="<?php trackback_url(); ?>" title="Send a notification when you link to this page">send a trackback</a> from your own site.</p>
+
+		<?php } elseif ( !comments_open() && pings_open() ) { ?>
+		<p>Comments are closed, but you can <a href="<?php trackback_url(); ?>" title="Send a notification when you link to this page">send a trackback</a> from your own site.</p>
+
+		<?php } elseif ( comments_open() && !pings_open() ) { ?>
+		<p><a href="#comment" title="Contribute to the discussion">Leave a comment</a>.</p>
+
+		<?php } elseif ( !comments_open() && !pings_open() ) { ?>
+		<p>Comments are closed.</p>
+
+		<?php } edit_post_link('Edit','<p>','</p>'); ?>
+
+	</article>
+
+<?php if (show_post_link_nav()): ?>
+	<p><?php previous_post_link('%link'); delim_post_link(); next_post_link('%link') ?></p>
+<?php endif; ?>
+
+<?php comments_template(); ?>
+
+<?php endwhile; else: ?>
+	<p>Sorry, no posts matched your criteria.</p>
+<?php endif; ?>
+
+</section>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
