e99a8adf6ac06cbde89c7db6e29fbd0797805188
Grant Hutchinson <grant@splorp.com>
2014-01-05 16:30:16 -0700

    Fixed responsive image scaling

--- image.php
+++ image.php
@@ -19,7 +19,7 @@
 		<h2 class="entry-title"><?php the_title(); ?></h2>
 		<p>By <span class="vcard author"><cite class="fn"><a class="url" href="<?php the_author_meta('user_url') ?>" title="Visit the author&#8217;s site"><?php the_author_meta('display_name'); ?></a></cite></span> in <a href="<?php echo get_permalink($post->post_parent); ?>" title="View the gallery"><?php echo get_the_title($post->post_parent); ?></a> on <span class="published" title="<?php the_time('c') ?>"><?php the_time(get_option('date_format')); ?></span></p>
 
-		<p><a href="<?php echo wp_get_attachment_url($post->ID); ?>"><?php echo wp_get_attachment_image( $post->ID, 'medium' ); ?></a></p>
+		<p><a href="<?php echo wp_get_attachment_url($post->ID); ?>"><?php echo wp_get_attachment_image( $post->ID, 'large' ); ?></a></p>
 		<?php
 			// Display the caption
 			if ( !empty($post->post_excerpt) ) the_excerpt();
